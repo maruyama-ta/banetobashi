a0b19f7fe84f34234e1d6d8f06a7272a4255a47a
tac <tac@sotai.co>
2024-12-05 10:24:08 +0900

    Init Project #1

---
+++ src/test/java/com/example/banetobashi/BanetobashiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banetobashi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanetobashiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
